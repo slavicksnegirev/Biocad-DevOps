8d3dd00fee9656965c350aa88f6cdcf4bb67dca2
Slavick <92212345+slavicksnegirev@users.noreply.github.com>
2024-11-12 11:28:54 +0300

    deployment

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "<h1>Hello, World!<h1>"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=32777)
